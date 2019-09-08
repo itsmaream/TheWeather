cbfdc5d26b24b059bf96910ede8200bbd6893a56
Muntadhar Haydar <muntadharhaydar@outlook.com>
2019-09-08 11:37:31 +0300

    Update packages and refine UI

--- TheWather/TheWather/Services/WebService.cs
+++ TheWather/TheWather/Services/WebService.cs
@@ -13,6 +13,9 @@ namespace TheWather.Services
     { 
         private static HttpClient client;
 
+        /// <summary>
+        /// The HttpClient to handle all our requests
+        /// </summary>
         public static HttpClient Client
         {
             get
@@ -22,12 +25,21 @@ namespace TheWather.Services
                 return client;
             }
         }
+
+        /// <summary>
+        /// call the API and get the data
+        /// </summary>
+        /// <param name="city">city name</param>
+        /// <returns>WeatherResponseRoot, null if something is not correct</returns>
         public static async Task <WeatherResponseRoot> GetWeatherAsync(string city)
         {
             try
             {
                 var resp = await Client.GetAsync($"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&APPID=75806ea8abd0d28aca0ce0c434fc04b7");
-                if (!resp.IsSuccessStatusCode) { return null; }
+                if (!resp.IsSuccessStatusCode)
+                {
+                    return null;
+                }
                 var str = await resp.Content.ReadAsStringAsync();
                 return JsonConvert.DeserializeObject<WeatherResponseRoot>(str);
             }
--- TheWather/TheWather/ViewModels/HomeViewModel.cs
+++ TheWather/TheWather/ViewModels/HomeViewModel.cs
@@ -7,46 +7,55 @@ using Xamarin.Forms;
 
 namespace TheWather.ViewModels
 {
-    public class HomeViewModel:BaseViewModel
+    /// <summary>
+    /// HomeViewModel
+    /// </summary>
+    public class HomeViewModel : BaseViewModel
     {
-        public HomeViewModel(INavigation navigation) : base(navigation)
-        {
-            GetWeatherResponseCommand = new Command(GetWeatherResponse);
-        }
-
-        
+        // have your fields and properties above the constructor
         private WeatherResponseRoot response;
         private string city;
-        private Weather weather;
 
+        /// <summary>
+        /// WeatherResponseRoot object
+        /// </summary>
         public WeatherResponseRoot Response
         {
             get => response;
             set => SetProperty(ref response, value);
         }
+        /// <summary>
+        /// City to search by
+        /// </summary>
         public string City
         {
             get => city;
             set => SetProperty(ref city, value);
         }
 
-        public Weather Weather
-        {
-            get => weather;
-            set => SetProperty(ref weather, value);
-        }
-        
+        public ICommand GetWeatherResponseCommand { get; }
 
+        public HomeViewModel(INavigation navigation) : base(navigation)
+        {
+            Title = "TheWather";
+
+            GetWeatherResponseCommand = new Command(GetWeatherResponse);
+        }
+
+        /// <summary>
+        /// Load WeatherResponse and set the object
+        /// </summary>
         private async void GetWeatherResponse()
         {
             if (IsBusy)
                 return;
+            // check if there's an actual input, if not, there is no need to search
+            if (string.IsNullOrEmpty(City))
+                return; // TODO:: notify the user
             IsBusy = true;
             Response = await Services.WebService.GetWeatherAsync(City);
-            Weather = Response.Weather[0];
             IsBusy = false;
         }
 
-        public ICommand GetWeatherResponseCommand { get; }
     }
 }
